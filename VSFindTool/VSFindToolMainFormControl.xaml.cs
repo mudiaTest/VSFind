04045374522c62ebff9614e0cdd333dba7ec53ae
lukasz <lukasz@biu.pl>
2017-04-19 09:30:45 +0200

    Inicjalizacja 2017

---
+++ VSFindTool/VSFindToolMainFormControl.xaml.cs
@@ -0,0 +1,178 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="VSFindToolMainFormControl.xaml.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.Shell;
+using EnvDTE;
+namespace VSFindTool
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for VSFindToolMainFormControl.
+    /// </summary>
+    public partial class VSFindToolMainFormControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VSFindToolMainFormControl"/> class.
+        /// </summary>
+        public VSFindToolMainForm parentToolWindow;
+        //List<string> resList;
+        public EnvDTE.Window LastDocWindow;
+        EnvDTE.DTE dte;
+        string originalFindResult2;
+
+		public VSFindToolMainFormControl()
+        {
+            this.InitializeComponent();
+            dte = (EnvDTE.DTE)Package.GetGlobalService(typeof(EnvDTE.DTE));
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(
+            //    string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
+            //    "VSFindToolMainForm");
+            TestSearch1();
+        }
+
+        public string GetFindResults2()
+        {
+            var findWindow = dte.Windows.Item(EnvDTE.Constants.vsWindowKindFindResults2);
+            var selection = findWindow.Selection as EnvDTE.TextSelection;
+            selection.SelectAll();
+            return selection.Text;
+
+            /*var endPoint = selection.AnchorPoint.CreateEditPoint();
+            endPoint.EndOfDocument();
+            return endPoint.GetLines(1, endPoint.Line);*/
+        }
+
+        public void HideFindResult2Window()
+        {
+            var findWindow = dte.Windows.Item(EnvDTE.Constants.vsWindowKindFindResults2);
+            findWindow.Visible = false; 
+        }
+
+        public void BringBackFindResult2Value()
+        {
+            var findWindow = dte.Windows.Item(EnvDTE.Constants.vsWindowKindFindResults2);
+            var selection = findWindow.Selection as EnvDTE.TextSelection;
+            selection.StartOfDocument();
+            selection.Delete(2);
+            //selection.Insert("rrr", 1);
+            var i = 0;
+        }
+
+        public void GetResults()
+        {
+            tbResult.Text = GetFindResults2();
+            List<string> list = tbResult.Text.Split('\n').ToList<string>();
+            if (list[list.Count - 2].StartsWith("Matching lines"))
+                BringBackFindResult2Value();
+            else
+                HideFindResult2Window();            
+        }
+
+        public void m_findEvents_FindDone(EnvDTE.vsFindResult Result, bool Cancelled)
+        {
+            GetResults();
+        }
+
+        public void m_WindowEvent(EnvDTE.Window GotFocus, EnvDTE.Window LostFocus)
+        {
+            foreach(EnvDTE.Document doc in dte.Documents)
+            {
+                foreach(EnvDTE.Window docWindow in doc.Windows)
+                {
+                    if (docWindow == GotFocus)
+                    {
+                        LastDocWindow = GotFocus;
+                        return;
+                    }
+                    
+                }
+            }
+        }
+
+        private void TestSearch1()
+        {
+            Boolean docSelected = true;
+            LastDocWindow = ((VSFindTool.VSFindToolPackage)(this.parentToolWindow.Package)).LastDocWindow;
+            tbResult.Text = "";
+            if (dte != null)
+            {
+                var m_findEvents = dte.Events.FindEvents;
+                m_findEvents.FindDone += new EnvDTE._dispFindEvents_FindDoneEventHandler(m_findEvents_FindDone);
+            }
+
+            Find find = dte.Find;
+            find.Action = vsFindAction.vsFindActionFindAll;
+            find.ResultsLocation = vsFindResultsLocation.vsFindResults2;
+
+            //Szukana fraza
+            find.FindWhat = tbPhrase.Text;
+
+            //Find options
+            find.MatchWholeWord = chkWholeWord.IsChecked == true;
+            find.MatchCase = chkCase.IsChecked == true;
+            if (chkRegExp.IsChecked == true)
+                find.PatternSyntax = vsFindPatternSyntax.vsFindPatternSyntaxRegExpr;
+            else
+                find.PatternSyntax = vsFindPatternSyntax.vsFindPatternSyntaxLiteral;
+
+            //Look in
+            if (rbCurrDoc.IsChecked == true)
+            {
+                find.Target = vsFindTarget.vsFindTargetCurrentDocument;
+
+                if (dte.ActiveDocument != null && dte.ActiveDocument.ActiveWindow != null)
+                {
+                    dte.ActiveDocument.ActiveWindow.Activate();
+                }
+                else if (LastDocWindow != null)
+                {
+                    LastDocWindow.Activate();
+                }
+                else
+                {
+                    tbResult.Text = "No document is active.";
+                    tbiLastResult.IsSelected = true;
+                    docSelected = false;
+                }
+            }
+            else if (rbOpenDocs.IsChecked == true)
+                find.Target = vsFindTarget.vsFindTargetOpenDocuments;
+            else if (rbProject.IsChecked == true)
+                find.Target = vsFindTarget.vsFindTargetCurrentProject;
+            else if (rbSolution.IsChecked == true)
+                find.Target = vsFindTarget.vsFindTargetSolution;
+
+             //find.SearchSubfolders = true;
+            var x = dte.Find.FindWhat;
+
+            originalFindResult2 = GetFindResults2();
+            if (docSelected)
+            {
+                var findWindow = dte.Windows.Item(EnvDTE.Constants.vsWindowKindFindResults2);                
+                vsFindResult result = find.Execute();
+                findWindow.Visible = false; 
+                tbiLastResult.IsSelected = true;
+                GetResults();
+            }
+        }
+    }
+}
---
+++ VSFindTool/VSFindToolPackage.cs
@@ -0,0 +1,104 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="VSFindToolPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace VSFindTool
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(VSFindToolPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(VSFindToolMainForm))]
+    public sealed class VSFindToolPackage : Package
+    {
+        /// <summary>
+        /// VSFindToolPackage GUID string.
+        /// </summary>
+        public const string PackageGuidString = "12f7a4f9-c3ad-468f-8c37-c35b41d4ed2f";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VSFindToolPackage"/> class.
+        /// </summary>
+        public VSFindToolPackage()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        public EnvDTE.Window LastDocWindow = null;
+        public void m_WindowActivatedEvent(EnvDTE.Window GotFocus, EnvDTE.Window LostFocus)
+        {
+            EnvDTE.DTE dte = (EnvDTE.DTE)Package.GetGlobalService(typeof(EnvDTE.DTE));   
+            foreach (EnvDTE.Document doc in dte.Documents)
+            {
+                foreach (EnvDTE.Window docWindow in doc.Windows)
+                {
+                    if (docWindow == GotFocus)
+                    {
+                        LastDocWindow = GotFocus;
+                        return;
+                    }
+
+                }
+            }
+        }
+
+        public void m_WindowClosingEvent(EnvDTE.Window Window)
+        {
+            if (LastDocWindow == Window)
+              LastDocWindow = null;
+        }
+
+        private void AddWindowEvent()
+        {
+            EnvDTE.DTE dte = (EnvDTE.DTE)Package.GetGlobalService(typeof(EnvDTE.DTE));
+            if (dte != null)
+            {
+                var m_WindowEvents = dte.Events.WindowEvents;
+                m_WindowEvents.WindowActivated += new EnvDTE._dispWindowEvents_WindowActivatedEventHandler(m_WindowActivatedEvent);
+                m_WindowEvents.WindowClosing += new EnvDTE._dispWindowEvents_WindowClosingEventHandler(m_WindowClosingEvent);
+            }
+        }
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+            VSFindToolMainFormCommand.Initialize(this);
+			AddWindowEvent();
+        }
+
+        #endregion
+    }
+}
