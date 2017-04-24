8616dc5b796e8692e64a4bc0d3b623c08660805f
lukasz <lukasz@biu.pl>
2017-04-24 13:51:32 +0200

    @work - przycisk flat/tree w trakcie

---
+++ VSFindTool/FindSettings.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+
+namespace VSFindTool
+{
+    class FindSettings
+    {
+        internal string tbPhrase;
+        internal bool chkWholeWord;
+        internal bool chkCase;
+        internal bool chkRegExp;
+        internal bool rbCurrDoc;
+        internal bool rbOpenDocs;
+        internal bool rbProject;
+        internal bool rbSolution;
+        internal vsFindAction action;
+        internal vsFindResultsLocation location;
+        //internal strind searchPath;
+
+        public void ToFind(Find find)
+        {
+            find.Action = action;
+            find.ResultsLocation = location;
+
+            //Search phrase
+            find.FindWhat = tbPhrase;
+
+            //Find options
+            find.MatchWholeWord = chkWholeWord;
+            find.MatchCase = chkCase;
+            if (chkRegExp)
+                find.PatternSyntax = vsFindPatternSyntax.vsFindPatternSyntaxRegExpr;
+            else
+                find.PatternSyntax = vsFindPatternSyntax.vsFindPatternSyntaxLiteral;
+
+            //Look in
+            if (rbCurrDoc)
+                find.Target = vsFindTarget.vsFindTargetCurrentDocument;           
+            else if (rbOpenDocs)
+                find.Target = vsFindTarget.vsFindTargetOpenDocuments;
+            else if (rbProject)
+                find.Target = vsFindTarget.vsFindTargetCurrentProject;
+            else if (rbSolution)
+                find.Target = vsFindTarget.vsFindTargetSolution;
+        }
+
+        public string ToLabelString()
+        {
+            string result = "";
+
+            if(chkWholeWord)
+                result += "W ";
+            else
+                result += "w ";
+
+            if (chkCase)
+                result += "C ";
+            else
+                result += "c ";
+
+            if (chkRegExp)
+                result += "R ";
+            else
+                result += "r ";
+
+            if (rbCurrDoc)
+                result +=  " [CurDocum] ";
+            else if (rbOpenDocs)
+                result += " [Opened] ";
+            else if (rbProject)
+                result += " [Project] ";
+            else if (rbSolution)
+                result += " [Solution] ";
+
+            result += "'" + tbPhrase + "'";     
+
+
+            return result;
+        }
+
+        public FindSettings GetCopy()
+        {
+            FindSettings result = new FindSettings();
+            result.tbPhrase = tbPhrase;
+            result.chkWholeWord = chkWholeWord;
+            result.chkCase = chkCase;
+            result.chkRegExp = chkRegExp;
+            result.rbCurrDoc = rbCurrDoc;
+            result.rbOpenDocs = rbOpenDocs;
+            result.rbProject = rbProject;
+            result.rbSolution = rbSolution;
+            result.action = action;
+            result.location = location;
+            return result;
+        }
+
+        public void SetColtrols(VSFindToolMainFormControl form)
+        {
+            //Search phrase
+            form.tbPhrase.Text = tbPhrase;
+
+            //Find options
+            form.chkWholeWord.IsChecked = chkWholeWord;
+            form.chkCase.IsChecked = chkCase;
+
+            //Look in
+            form.rbCurrDoc.IsChecked = rbCurrDoc;
+            form.rbOpenDocs.IsChecked = rbOpenDocs;
+            form.rbProject.IsChecked = rbProject;
+            form.rbSolution.IsChecked = rbSolution;
+        }
+    }
+}
--- VSFindTool/VSFindToolMainFormControl.xaml.cs
+++ VSFindTool/VSFindToolMainFormControl.xaml.cs
@@ -14,6 +14,7 @@ namespace VSFindTool
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
 
     /// <summary>
     /// Interaction logic for VSFindToolMainFormControl.
@@ -28,6 +29,7 @@ namespace VSFindTool
         public EnvDTE.Window LastDocWindow;
         EnvDTE.DTE dte;
         string originalFindResult2;
+        Dictionary<string, FindSettings> findSettings = new Dictionary<string, FindSettings>();
 
         ResultTreeViewModel resultTree;
 
@@ -133,53 +135,113 @@ namespace VSFindTool
             //add main grid in tab and row definitions
             Grid grid = new Grid() {
                 Background = this.FindResource(SystemColors.ControlLightBrushKey) as Brush };
-            grid.RowDefinitions.Add(new RowDefinition() {
-                Height = new GridLength(30, GridUnitType.Pixel) });
-            grid.RowDefinitions.Add(new RowDefinition() {
+            grid.RowDefinitions.Add(new RowDefinition()
+            {
+                Height = new GridLength(18, GridUnitType.Pixel)
+            });
+            grid.RowDefinitions.Add(new RowDefinition()
+            {
+                Height = new GridLength(30, GridUnitType.Pixel)
+            });
+            grid.RowDefinitions.Add(new RowDefinition()
+            {
                 Name = snapshotTag + "_rowFlat",
-                Height = new GridLength(1, GridUnitType.Star) });
-            grid.RowDefinitions.Add(new RowDefinition() {
+                Height = new GridLength(1, GridUnitType.Star)
+            });
+            grid.RowDefinitions.Add(new RowDefinition()
+            {
                 Name = snapshotTag + "_rowTree",
-                Height = new GridLength(0, GridUnitType.Pixel) });
+                Height = new GridLength(0, GridUnitType.Pixel)
+            });
+            grid.RowDefinitions.Add(new RowDefinition()
+            {
+                Name = snapshotTag + "_preview",
+                Height = new GridLength(60, GridUnitType.Pixel)
+            });
             //add grid as the main element in tab
             newTab.Content = grid;
             //Set snapshot tab as selected
             //tbcMain.SelectedItem = newTab;
 
-            //upper menu wrap panel
-            WrapPanel upperMenyWrapPanel = new WrapPanel() {
-                Orientation = Orientation.Horizontal,
+            //Info label
+            Label infoLabel = new Label()
+            {
                 HorizontalAlignment = HorizontalAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Top };           
-            Grid.SetRow(upperMenyWrapPanel, 0);
+                VerticalAlignment = VerticalAlignment.Top,
+                Padding = new Thickness(5, 0, 5, 5),
+                Content = last_searchSettings.ToLabelString()
+            };
+            grid.Children.Add(infoLabel);
+            Grid.SetRow(infoLabel, 0);
+
+            //upper menu wrap panel
+            WrapPanel upperMenuWrapPanel = new WrapPanel()
+            {
+                Orientation = Orientation.Horizontal,
+            };
+            grid.Children.Add(upperMenuWrapPanel);
+            Grid.SetRow(upperMenuWrapPanel, 1);
+
+                //toggle button
+                ToggleButton tbFlatTree = new ToggleButton()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Margin = new Thickness(5, 3, 0, 0),
+                    Padding = new Thickness(3, 1, 3, 1),
+                    Width = 29,
+                    BorderBrush = this.FindResource(SystemColors.ControlDarkBrushKey) as Brush,
+                    Height = 21
+                };            
+                /*Style tbStyle = new Style(typeof(ToggleButton))
+                {
+
+                };
+                tbFlatTree.Style = tbStyle;*/
+                //DependencyProperty dpStyle = new DependencyProperty();
+                /* Setter tbSetter = new Setter()
+                 {
+                     //Property = dpStyle
+                 };
+                 tbSetter.Property = new
+                 tbStyle.Setters.Add(tbSetter);*/
+                tbFlatTree.Content = "Flat";
+            upperMenuWrapPanel.Children.Add(tbFlatTree);
+
 
             //add border and treeview for Flat view
-            Border borderFlat = new Border() {
+            Border borderFlat = new Border()
+            {
                 BorderBrush = Brushes.Black,
                 BorderThickness = new Thickness(1),
                 Margin = new Thickness(0),
-                RenderTransformOrigin = new Point(0.5, 0.5) };
+                RenderTransformOrigin = new Point(0.5, 0.5)
+            };
             grid.Children.Add(borderFlat);
-            Grid.SetRow(borderFlat, 1);
+            Grid.SetRow(borderFlat, 2);
             Grid.SetColumnSpan(borderFlat, 1);
-            TreeView flattv = new TreeView() {
+            TreeView flattv = new TreeView()
+            {
                 Name = snapshotTag + "_tvResultFlatTree",
                 HorizontalContentAlignment = HorizontalAlignment.Stretch
             };
             borderFlat.Child = flattv;
 
             //add border and treeview for Tree view
-            Border borderTree = new Border() {
+            Border borderTree = new Border()
+            {
                 BorderBrush = Brushes.Black,
                 BorderThickness = new Thickness(1),
                 Margin = new Thickness(0),
-                RenderTransformOrigin = new Point(0.5, 0.5) };
+                RenderTransformOrigin = new Point(0.5, 0.5)
+            };
             grid.Children.Add(borderTree);
-            Grid.SetRow(borderTree, 2);
+            Grid.SetRow(borderTree, 3);
             Grid.SetColumnSpan(borderTree, 2);
-            TreeView treetv = new TreeView() {
+            TreeView treetv = new TreeView()
+            {
                 Name = snapshotTag + "_tvResultFlatTree",
-                HorizontalContentAlignment = HorizontalAlignment.Stretch };
+                HorizontalContentAlignment = HorizontalAlignment.Stretch
+            };
             borderTree.Child = treetv;
 
             //Populate new TreeViews from "last"
@@ -187,6 +249,26 @@ namespace VSFindTool
             //Expand new views
             SetExpandAllInLvl(flattv.Items, true);
             SetExpandAllInLvl(treetv.Items, true);
+
+            findSettings.Add(snapshotTag ,last_searchSettings.GetCopy());
+
+
+            tbFlatTree.Click += (o, e) =>
+            {
+                ToggleButton tb = o as ToggleButton;
+                if (tb.IsChecked == false)
+                {
+                    flattv.Height = Double.NaN;
+                    treetv.Height = 0;
+                    tb.Content = "Tree";
+                }
+                else
+                {
+                    flattv.Height = 0;
+                    treetv.Height = Double.NaN;
+                    tb.Content = "Flat";
+                }
+            };
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -195,6 +277,7 @@ namespace VSFindTool
             //Todo - dodać nową zakładkę o nazwie odebranej od uzytkowniak
             //TODO dodać na zakładkę nowe obiekty
             //todo dodać navigatora
+            //to do dodać FILL setting from settings
             //Todo podłaczyć do obiektów eventy
             //todo dodać skrót wlaczający tool na pierwszą zakładkę
         }
